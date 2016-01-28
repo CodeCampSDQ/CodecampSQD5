1aaacc80fbffe4f0d15b4c427f07f48ec45cf755
Luis Alberto Pena Nunez <luis.pena.nunez.developer@gmail.com>
2016-01-28 13:11:01 -0600

    Added missing file apply

---
+++ CodecampSDQ2016/CodecampSDQ2016/AppKeys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodecampSDQ2016
+{
+	public class AppKeys
+	{
+		public static readonly string TWITTER_CONSUMER_KEY = "";
+
+		public static readonly string TWITTER_CONSUMER_SECRET = "";
+
+		public static readonly string TWITTER_ACCESSTOKEN = "";
+
+		public static readonly string TWITTER_ACESSTOKENSECRET = "";
+	}
+}
+
